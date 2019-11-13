8889ff26d680664b2b8f2e9e87375ae50599e075
Joey Nelson <JPN@swc.com>
2019-11-13 17:05:29 -0600

    Updating the application to allow name changing from typing in the textboxes using latest Javascript standards

--- src/App.js
+++ src/App.js
@@ -19,15 +19,22 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { id: 1, name: 'Joe', age: 28 },
-        { id: 2, name: event.target.value, age: 29 },
-        { id: 3, name: 'Alex', age: 27 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((person) => {
+      return person.id === id;
     });
-  };
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -52,9 +59,10 @@ class App extends Component {
             return(
               <Person
                 key={person.id}
-                click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             )
           })}
@@ -64,7 +72,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <h1>This is my React App!</h1>
         <p>This is really working!</p>
         <button 
           style={style}
