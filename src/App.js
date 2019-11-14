3f5e72912edb96c33a7929746f5e37432330ee46
Joey Nelson <JPN@swc.com>
2019-11-14 11:55:16 -0600

    Using styled components instead of Radium

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -105,4 +104,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
