45d8559832aaeab4b89849414f8e45e9a140ea4a
Joey Nelson <JPN@swc.com>
2019-11-15 15:28:17 -0600

    Adding Auxiliary higher order component to combat the javascript issue of returning multiple parallel elements in JSX (Two divs not nested)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import Auxiliary from '../../../hoc/Auxiliary';
+import auxiliary from '../../../hoc/Auxiliary';
 
 class Person extends Component{
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>My name is {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
+            <Auxiliary>
+                <div className={classes.Person}>
+                    <p onClick={this.props.click}>My name is {this.props.name} and I am {this.props.age} years old!</p>
+                    <p>{this.props.children}</p>
+                    <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+                </div>
+            </Auxiliary>
         )
     }
 }
